c16a9bd07361d81fd2f393a4127e6202f553df27
Caruso13 <157031605+Caruso13@users.noreply.github.com>
2024-06-12 11:00:18 -0300

    Add files via upload

---
+++ exercicio.ts
@@ -0,0 +1,12 @@
+function multiplicacao(x: number, y: number): number {
+    return x * y;
+}
+
+
+function saudacao(nome: string): string {
+    return "Olá " + nome +" seja bem vindo(a)";
+}
+
+
+console.log(multiplicacao(22, 365));
+console.log(saudacao("Maria")); 
